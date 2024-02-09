834f8f0be8b9ae5ec291ddef9e2c9e30d129e39b
alexisyanez <adanilo.yanez@gmail.com>
2024-02-09 16:35:29 +0100

    getting traces for SUMO intersection and obstacles losses updated from dBm to mW

--- OOP_for_SPS/adding_type.py
+++ OOP_for_SPS/adding_type.py
@@ -6,33 +6,45 @@ from random import randrange
 import csv
 
 #print(randrange(10))
+#sumo_ped_vehicle_location_sec_
+#/home/simu5g/Simulators-for-SPS/OOP_for_SPS/traffic_data_ped/
 time_period_all=300000
 
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 for section_index in range(0,int(time_period_all/10000)):
     #location_file_name = 'sumo_vehicle_location_'+ str(section_index)
-    location_file_name = 'manhattan_location_s20_'+ str(section_index)
+    #location_file_name = 'manhattan_location_s20_'+ str(section_index)
     #location_file_name = 'sumo_vehicle_location' # + str(section_index)
+    location_file_name = 'sumo_ped_vehicle_location_sec_' + str(section_index) 
     print('section_index',section_index)
-    Data=np.array(pd.read_csv("C:/Users/adani/OneDrive/Documentos/GitHub/SimulatorSPS/OOP_for_SPS/traffic_data_v2/%s.csv"%(location_file_name),header=None)).tolist()
+    #Data=np.array(pd.read_csv("C:/Users/adani/OneDrive/Documentos/GitHub/SimulatorSPS/OOP_for_SPS/traffic_data_v2/%s.csv"%(location_file_name),header=None)).tolist()
+    Data=np.array(pd.read_csv("/home/simu5g/Simulators-for-SPS/OOP_for_SPS/traffic_data_ped/%s.csv"%(location_file_name),header=None)).tolist()
     NewData=[]
     for i in range(0,len(Data)):
         p=randrange(10)
         type=[]
         #print(p) 
-        if p>=0 and p<2:   
-            type=1
-        elif p>=2 and p<6:   
-            type=2
-        else:   
-            type=3
+        #if p>=0 and p<2:   
+        #    type=1
+        #elif p>=2 and p<6:   
+        #    type=2
+        #else:   
+        #    type=3
+        if Data[i][3] == "DEFAULT_PEDTYPE" or Data[i][3] == "motorcycle" or Data[i][3] == "moped" or Data[i][3] == "slowbicycle" or Data[i][3] == "fastbicycle" or Data[i][3] == "avgbicycle":  
+            type=1 #Type 1 corresponds to VRU
+        else:
+            type=2 #Type 2 corresponds to car
         NewData.append([Data[i][0],Data[i][1],Data[i][2],type])
     
-    filename='v2manhattan_location_s20_'+str(section_index)
+    #filename='v2manhattan_location_s20_'+str(section_index)
+    filename='v2sumo_ped_vehicle_location_sec_'+str(section_index)
     n=0
-    f=open("C:/Users/adani/OneDrive/Documentos/GitHub/SimulatorSPS/OOP_for_SPS/traffic_data_v2/%s.csv"%filename,'w',newline='')
+    #f=open("C:/Users/adani/OneDrive/Documentos/GitHub/SimulatorSPS/OOP_for_SPS/traffic_data_v2/%s.csv"%filename,'w',newline='')
+    f=open("/home/simu5g/Simulators-for-SPS/OOP_for_SPS/traffic_data_ped/%s.csv"%filename,'w',newline='')
+    
     writer=csv.writer(f)
+    
     for j in NewData:
         writer.writerow(j)
     f.close()
