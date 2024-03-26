6e111c5a4be20ba145e0dd4d45a5b09e879a79f2
Alexis Yanez <adanilo.yanez@gmail.com>
2024-03-26 11:35:01 -0300

    Subiendo resultados grupo1 20Hz y Grupo 2 50Hz

--- OOP_for_SPS/runs.py
+++ OOP_for_SPS/runs.py
@@ -1,4 +1,4 @@
-import multiprocessing 
+import multiprocessing
 import json
 import SPS_test
 import numpy as np
@@ -6,13 +6,14 @@ import tqdm
 
 def wrapper(args):
     # Función auxiliar para llamar a SPS_test.main con los argumentos
+    # self.resultados.append(list(tqdm.tqdm(pool.imap_unordered(wrapper,param_list[:8]), total=len(param_list[:8])))
     return SPS_test.main(*args)
 
 if __name__ == "__main__":
 
-    time_period = 5200    
+    time_period = 10201    
     start_sampling_time = 200
-    interval = 50
+    interval = 100
     RC_low = 5
     RC_high = 15
     RSRP_ratio_beacon = 0.2      
@@ -43,25 +44,38 @@ if __name__ == "__main__":
     
     # Número máximo de procesos (8 para usar 8 CPU)
     num_procesos = 8
-
+    resultados=[]
     # Crear un grupo de procesos
     with multiprocessing.Pool(processes=num_procesos) as pool:
         #resultados = pool.map(wrapper,param_list)
-        resultados = list(tqdm.tqdm(pool.imap_unordered(wrapper,param_list), total=len(param_list)))
+        resultados.extend(list(tqdm.tqdm(pool.imap_unordered(wrapper,param_list), total=len(param_list))))
+
+    diccionario_final = {}
+    for idx, resultado in enumerate(resultados):
+        resultado_dic = json.loads(resultado)
+        diccionario_final[idx] = resultado_dic
+
+    with open("Final_results_group3_10Hz.json", "w") as archivo:
+        json.dump(diccionario_final, archivo, indent=4)
+
+    print("Archivo 'Final_results_group3_10Hz.json' creado exitosamente.")
+
+#    print("Archivo 'Final_results_group1.json' creado exitosamente.")
 
     # Ahora 'resultados' contiene los 90 resultados generados en paralelo
     # Puedes procesarlos o combinarlos según lo que necesites
 
     # Por ejemplo, imprimir los primeros 10 resultados
-    print(resultados[:10])
+    #print(resultados[:10])
 
     # Combinar los resultados en un solo diccionario
-    diccionario_final = {}
-    for resultado in resultados:
-        diccionario_final.update(resultado)
+    #diccionario_final = {}
+    #for resultado in resultados:
+    #    resultado_dic = json.loads(resultado)
+    #    diccionario_final.update(resultado_dic)
 
     # Exportar como archivo JSON
-    with open("Final_results_group1.json", "w") as archivo:
-        json.dump(diccionario_final, archivo, indent=4)
+    #with open("Final_results_group1.json", "w") as archivo:
+    #    json.dump(resultados, archivo, indent=4)
 
-    print("Archivo 'Final_results_group1.json' creado exitosamente.")
+    #print("Archivo 'Final_results_group1.json' creado exitosamente.")
